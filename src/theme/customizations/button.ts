f8af93358b35f7cb0dab675ffb3574bc9fd8578f
David <102764748+davidbutsch@users.noreply.github.com>
2025-06-09 22:27:58 -0500

    update button style

--- src/theme/customizations/button.ts
+++ src/theme/customizations/button.ts
@@ -12,6 +12,7 @@ export const buttonCustomizations: Components<Theme> = {
       root: {
         flex: "none", // fix text wrapping issue
         fontWeight: 600, // all buttons have bold text
+        textTransform: "none",
 
         variants: [
           // CONTAINED VARIANT
@@ -27,11 +28,7 @@ export const buttonCustomizations: Components<Theme> = {
           {
             props: { variant: "text" },
             style: {
-              "&:hover": {
-                background: "transparent",
-                textDecoration: "underline",
-                textDecorationThickness: "2px",
-              },
+              "&:hover": {},
             },
           },
           // LARGE SIZE

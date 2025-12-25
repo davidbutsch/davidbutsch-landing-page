83ef693eb847aabedc275be6ede55ec9f67cf2ae
David <102764748+davidbutsch@users.noreply.github.com>
2025-12-24 19:28:46 -0600

    only display blog promotional if entries exist

--- src/modules/home/components/BlogPromotional/CardsPreview.tsx
+++ src/modules/home/components/BlogPromotional/CardsPreview.tsx
@@ -14,30 +14,29 @@ export const CardsPreview = () => {
   });
   const posts = getAllPostsQuery.data?.slice(0, 6);
 
-  if (posts?.length == 0) return;
-
-  return (
-    <Stack alignItems="center">
-      <Typography variant="h4" textAlign="center" mb={6}>
-        Check out the Blog
-      </Typography>
-      <Box
-        sx={{
-          position: "relative",
-          maskImage: "linear-gradient(to bottom, #FFFFFF, transparent)",
-        }}
-      >
-        <Grid container spacing={2}>
-          {posts?.map((post) => (
-            <Grid key={post.id} size={{ xs: 12, sm: 6, md: 4 }}>
-              <PostCard postId={post.id} jotId={post.jotId} />
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
-      <Button onClick={onReadMore} color="inherit" sx={{ px: 2 }}>
-        Read More
-      </Button>
-    </Stack>
-  );
+  if (posts?.length && posts.length != 0)
+    return (
+      <Stack alignItems="center">
+        <Typography variant="h4" textAlign="center" mb={6}>
+          Check out the Blog
+        </Typography>
+        <Box
+          sx={{
+            position: "relative",
+            maskImage: "linear-gradient(to bottom, #FFFFFF, transparent)",
+          }}
+        >
+          <Grid container spacing={2}>
+            {posts?.map((post) => (
+              <Grid key={post.id} size={{ xs: 12, sm: 6, md: 4 }}>
+                <PostCard postId={post.id} jotId={post.jotId} />
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+        <Button onClick={onReadMore} color="inherit" sx={{ px: 2 }}>
+          Read More
+        </Button>
+      </Stack>
+    );
 };
